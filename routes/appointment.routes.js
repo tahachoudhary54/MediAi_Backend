0bcdd2a39f3840e3525b6a3573d9a18b1e468606
Taha Choudhary <tahachoudhary54@gmail.com>
2026-05-22 17:06:39 +0530

    Ui chnaged

--- routes/appointment.routes.js
+++ routes/appointment.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createAppointment, getPatientAppointments, getDoctorAppointments, updateAppointment, deleteAppointment } from '../controllers/appointment.controller.js';
+import { createAppointment, getPatientAppointments, getDoctorAppointments, updateAppointment, deleteAppointment, getDoctorPatients, getDoctorPatientDetail } from '../controllers/appointment.controller.js';
 import { protect } from '../middleware/auth.middleware.js';
 import { authorize } from '../middleware/role.middleware.js';
 
@@ -10,6 +10,8 @@ router.use(protect);
 router.post('/', createAppointment);
 router.get('/patient', authorize('patient'), getPatientAppointments);
 router.get('/doctor', authorize('doctor'), getDoctorAppointments);
+router.get('/doctor/patients', authorize('doctor'), getDoctorPatients);
+router.get('/doctor/patient/:patientId', authorize('doctor'), getDoctorPatientDetail);
 router.route('/:id')
     .put(updateAppointment)
     .delete(deleteAppointment);
